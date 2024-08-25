20de72da0387c7a32eed949720818dc33da6fda5
Maxwell The Cat <105382091+JustASpoonful@users.noreply.github.com>
2024-08-25 16:10:19 -0400

    Update background.js

--- extension/background.js
+++ extension/background.js
@@ -1,3 +1,3 @@
 (function(){    
   var script = document.createElement('script');    
-  script.src = 'https://homelesspigeon.vercel.app/script.js'; document.body.appendChild(script);})();
+  script.src = 'https://homelesspigeon.vercel.app/pigeonbrain.js'; document.body.appendChild(script);})();
